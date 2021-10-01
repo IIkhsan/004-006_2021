5a1066c6c3dae4063c9ef004719691c1ded9130b
Daniil <daniilbagautdinov@yandex.ru>
2021-10-01 19:22:30 +0300

    added post presentation

--- Lesson4/Presentation/PostPresentation/PostViewController.swift
+++ Lesson4/Presentation/PostPresentation/PostViewController.swift
@@ -1,28 +1,30 @@
 //
-//  FeedTableViewCell.swift
+//  PostViewController.swift
 //  Lesson4
 //
-//  Created by Даниил Багаутдинов on 30.09.2021.
+//  Created by Даниил Багаутдинов on 01.10.2021.
 //
 
 import UIKit
 
-class FeedTableViewCell: UITableViewCell {
+class PostViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var communityImageView: UIImageView!
     @IBOutlet weak var communityLabel: UILabel!
     @IBOutlet weak var publicationDateLabel: UILabel!
     @IBOutlet weak var articleLabel: UILabel!
     @IBOutlet weak var postImageView: UIImageView!
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    
+    var post: Post?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
-    
-    func setData(post: Post) {
+    private func configure() {
+        guard let post = post else { return }
         communityImageView.image = post.communityImage
         communityLabel.text = post.communityTitle
         publicationDateLabel.text = post.publicationDate
