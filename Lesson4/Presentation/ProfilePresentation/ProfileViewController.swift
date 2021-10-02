8934e3e320123a86bc66c6a3a31aa1c71ebf111a
Daniil <daniilbagautdinov@yandex.ru>
2021-10-02 20:53:13 +0300

    added profile presentation

---
+++ Lesson4/Presentation/ProfilePresentation/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Lesson4
+//
+//  Created by Даниил Багаутдинов on 02.10.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var educationLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var workLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func editInformation(_ sender: Any) {
+    }
+    
+    @IBAction func detailedInformation(_ sender: Any) {
+    }
+}
