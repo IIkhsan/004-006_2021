e10e1ec896153adb098ecb5854f46c7f63d04c20
Ruslan <ruslangazizov36@gmail.com>
2021-10-06 20:52:10 +0300

    Add basic project structure

---
+++ Lesson4/Login/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Lesson4
+//
+//  Created by Руслан on 06.10.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func didTapLoginButton(_ sender: UIButton) {
+        
+    }
+    
+}
