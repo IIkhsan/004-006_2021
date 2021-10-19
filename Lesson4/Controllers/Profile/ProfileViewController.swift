66c69b3714a40bfdb6a49a8a30caf3bc186a9d40
rustemd02 <rustemd02@mail.ru>
2021-10-19 23:47:42 +0300

    UI changes

--- Lesson4/Controllers/Profile/ProfileViewController.swift
+++ Lesson4/Controllers/Profile/ProfileViewController.swift
@@ -20,6 +20,7 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         profileImageView.image = data.currentUser.profileImage
+        profileImageView.layer.cornerRadius = 30
     }
     
     override func viewDidAppear(_ animated: Bool) {
