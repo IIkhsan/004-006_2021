420e10ed6169e3beeb82fa8512f50b059c0c092a
rustemd02 <rustemd02@mail.ru>
2021-10-19 23:03:55 +0300

    Delegate and layout fixes

--- Lesson4/Controllers/Feed/ViewController.swift
+++ Lesson4/Controllers/Feed/ViewController.swift
@@ -9,19 +9,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.tableView.register(UINib.init(nibName: "TextTableViewCell", bundle: nil), forCellReuseIdentifier: "TextTableViewCell")
-        self.tableView.register(UINib.init(nibName: "ImageTableViewCell", bundle: nil), forCellReuseIdentifier: "ImageTableViewCell")
-        self.tableView.register(UINib.init(nibName: "TextAndImageTableViewCell", bundle: nil), forCellReuseIdentifier: "TextAndImageTableViewCell")
-
-        configure()
-    }
-    
-    private func configure() {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.reloadData()
     }
+    
 }
 
 // MARK: - Table view data source & delegate
@@ -34,23 +26,12 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let post = posts[indexPath.row]
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TextAndImageTableViewCell", for: indexPath) as? TextAndImageTableViewCell  else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as? TextAndImageTableViewCell  else { return UITableViewCell() }
         cell.configure(post: post)
         return cell
     }
     
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        let post = posts[indexPath.row]
-//
-//        if (post.content != nil) && (post.contentPicture != nil){
-//            return 400
-//        }
-//        if post.content != nil{
-//            return 100
-//        }
-//        return 300
-//    }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let post = posts[indexPath.row]
